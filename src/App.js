08c60701c7c9834a1181ed389d2ee9a669fc90a1
Krzysztof <k.ukleja@gmail.com>
2019-05-19 23:22:17 +0200

    State (useState)in Functional Component - introduced to new React.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-    state = {
+const App = props => {
+
+    const [ personsState, setPersonsState ] = useState({
         persons: [
             {name: 'Adam', age: '31'},
             {name: 'Ewa', age: '30'}
         ]
+    });
+
+    const switchNameHandler = () => {
+        setPersonsState({
+                persons: [
+                    {name: 'Adam', age: '32'},
+                    {name: 'Ewa', age: '31'}
+                ]
+            }
+        )
     }
 
-    // It is an event handler. Good practice: give name ending handler
-    switchNameHandler = () => {
-        console.log('Was clicked');
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+            {/*switchNameHandler is without (), because with () this.switchNameHandler() it will execute immediately*/}
+            {/*Without () is a reference*/}
+            <button onClick={switchNameHandler}>Switch name</button>
+            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}> My hobby is sth.</Person>
+            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}></Person>
+        </div>
+    );
 
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <h1>Hello</h1>
-                {/*switchNameHandler is without (), because with () this.switchNameHandler() it will execute immediately*/}
-                {/*Without () is a reference*/}
-                <button onClick={this.switchNameHandler}>Switch name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> My hobby is sth.</Person>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
-            </div>
-        );
-    }
 }
 
 export default App;
