8b126f665b1e98b7bc8b821bc421edbddae84f14
Krzysztof <k.ukleja@gmail.com>
2019-05-20 22:32:28 +0200

    State (useState)in Functional Component - add comment

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Person from './Person/Person';
 
 const App = props => {
 
-    const [ personsState, setPersonsState ] = useState({
+    // Second parameter setPersonState - by this name I can set new state
+    // example in switchNameHandler
+    const [personsState, setPersonsState] = useState({
         persons: [
             {name: 'Adam', age: '31'},
+            {name: 'Celina', age: '32'},
             {name: 'Ewa', age: '30'}
         ]
     });
@@ -15,6 +18,7 @@ const App = props => {
         setPersonsState({
                 persons: [
                     {name: 'Adam', age: '32'},
+                    {name: 'Celina', age: '33'},
                     {name: 'Ewa', age: '31'}
                 ]
             }
@@ -27,8 +31,20 @@ const App = props => {
             {/*switchNameHandler is without (), because with () this.switchNameHandler() it will execute immediately*/}
             {/*Without () is a reference*/}
             <button onClick={switchNameHandler}>Switch name</button>
-            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}> My hobby is sth.</Person>
-            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}></Person>
+            <Person
+                name={personsState.persons[0].name}
+                age={personsState.persons[0].age}> My hobby is sth.
+            </Person>
+            <Person
+                name={personsState.persons[1].name}
+                age={personsState.persons[1].age}
+                // click={switchNameHandler()}
+            >
+            </Person>
+            <Person
+                name={personsState.persons[2].name}
+                age={personsState.persons[2].age}>
+            </Person>
         </div>
     );
 
